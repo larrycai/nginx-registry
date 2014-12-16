de608e3dd98d421f0a31858f2ac6d1021b55b3c1
Larry Cai <larry.caiyu@gmail.com>
2014-12-16 11:23:35 +0800

    copy to default place

--- start.sh
+++ start.sh
@@ -9,13 +9,14 @@ cp /app/nginx/docker-registry.default ${NGINX}/conf/docker-registry.default
 if [ -d ${DATA} ];
 then
     echo "welcome to nginx-registry"
+	cp /data/docker-registry.htpasswd ${NGINX}/conf
 else
     echo "use default for demo purpose !!"
 	mkdir -p ${DATA}
 	mkdir -p /conf
 	cp /app/nginx/server.crt ${DATA}/
 	cp /app/nginx/server.key ${DATA}/
-	cp /app/nginx/docker-registry.htpasswd ${DATA}/
+	cp /app/nginx/docker-registry.htpasswd ${NGINX}/conf
 fi
 
 ${NGINX}/sbin/nginx
